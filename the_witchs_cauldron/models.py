911dcbd86ebc9ca68d2a2a55e9ca559ba60c197d
rachel-o-donnell <hellotherachelo@gmail.com>
2023-01-04 14:56:22 +0000

    Add PostSpell class to models.py

--- the_witchs_cauldron/models.py
+++ the_witchs_cauldron/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+# Post a Spell
+
+class PostSpell(models.Model):
+    title = models.Charfield(max_length=250, unique=True)
+    slug = models.Slugfield(max_length-250, unique=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='spell_posts')
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    main_image = CloudinaryField('image', default='placeholder')
+    description = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='spell_likes', blank=True)
+
+    # orders spell posts by most recently created (minus if for decending order)
+    class Meta:
+        ordering = ['-created_on']
+
+    # returns a string representaion of an object
+    def __str__(self):
+        return self.title
+
+    # returns the number of likes on a spell post
+    def no_of_likes(self):
+        return self.likes.count()
