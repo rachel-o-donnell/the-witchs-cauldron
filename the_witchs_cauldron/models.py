f95b1dadfa85751abdc46794059203a2a7102d64
rachel-o-donnell <hellotherachelo@gmail.com>
2023-01-04 15:34:46 +0000

    Add comment class to models.py

--- the_witchs_cauldron/models.py
+++ the_witchs_cauldron/models.py
@@ -6,9 +6,10 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 # Post a Spell
 
+
 class PostSpell(models.Model):
     title = models.Charfield(max_length=250, unique=True)
-    slug = models.Slugfield(max_length-250, unique=True)
+    slug = models.Slugfield(max_length=250, unique=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='spell_posts')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
@@ -29,3 +30,22 @@ class PostSpell(models.Model):
     # returns the number of likes on a spell post
     def no_of_likes(self):
         return self.likes.count()
+
+# Comments
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(PostSpell, on_delete=models.CASCADE, related_name='comments')
+    username = models.CharField(max_length=70)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    # orders comments by most recently commented
+    class Meta:
+        ordering = ['created_on']
+
+    # returns a string representaion of an object
+    def __str__(self):
+        return f"Comment {self.body} by {self.username}"
