1f846beed035401cbf0100cb816ed59772daee62
rachel-o-donnell <hellotherachelo@gmail.com>
2023-01-04 15:50:21 +0000

    Fix bug#2 in PostSpell/models.py
    missing capital F in CharField and SlugField

--- the_witchs_cauldron/models.py
+++ the_witchs_cauldron/models.py
@@ -8,8 +8,8 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class PostSpell(models.Model):
-    title = models.Charfield(max_length=250, unique=True)
-    slug = models.Slugfield(max_length=250, unique=True)
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='spell_posts')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
