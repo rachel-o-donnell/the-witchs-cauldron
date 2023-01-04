4b0cc8b34e63d6d589c2a5fb6ccae3d83f56cc1c
rachel-o-donnell <hellotherachelo@gmail.com>
2023-01-04 17:48:40 +0000

    Add summernote to admin panel  and migrate

--- the_witchs_cauldron/admin.py
+++ the_witchs_cauldron/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import PostSpell
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(PostSpell)  # Decorator: Pythonic way of registering models from models.py to admin panel
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
