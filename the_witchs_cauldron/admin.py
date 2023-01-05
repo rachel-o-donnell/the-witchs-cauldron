7be2021acf99f4b7f44eed06ce8438de49a09f92
rachel-o-donnell <hellotherachelo@gmail.com>
2023-01-05 14:20:51 +0000

    Add expanded features in PostAdmin/admin.py

--- the_witchs_cauldron/admin.py
+++ the_witchs_cauldron/admin.py
@@ -3,7 +3,12 @@ from .models import PostSpell
 from django_summernote.admin import SummernoteModelAdmin
 
 
+# Summernote features for admin panel
 @admin.register(PostSpell)  # Decorator: Pythonic way of registering models from models.py to admin panel
 class PostAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}  # Prepopulates slugfield in admin panel
     summernote_fields = ('content')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
