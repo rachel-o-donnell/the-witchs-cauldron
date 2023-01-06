b9e9522bb15bfaab63925f276ea7335172a7a16c
rachel-o-donnell <hellotherachelo@gmail.com>
2023-01-06 14:48:39 +0000

    Add basic PostList class to views.py

--- the_witchs_cauldron/views.py
+++ the_witchs_cauldron/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import PostSpell
 
-# Create your views here.
+
+# class based views allows for re-use - One view can inherit from another
+class PostList(generic.ListView):
+    model = PostSpell()
+    queryset = PostSpell.objects.filter(status=1).order_by('created_on')
+    template_name = 'index.html'
+    paginate_by = 3  # can only view this many at a time before adding a page nav-change to 9/continuous?
