a5795806f017b92198a53de56bc55dc6c95dbe9b
rachel-o-donnell <hellotherachelo@gmail.com>
2023-01-11 18:40:00 +0000

    Add url path for SpellDetail in urls.py

--- the_witchs_cauldron/urls.py
+++ the_witchs_cauldron/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home')
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.SpellDetail.as_view(), name='spell_detail'),
 ]
