c7119d1a75cfbca2204bb2b08aba4df8bd59b7c2
rachel-o-donnell <hellotherachelo@gmail.com>
2023-01-17 22:31:43 +0000

    Add post function in spell_detail in views.py

--- the_witchs_cauldron/views.py
+++ the_witchs_cauldron/views.py
@@ -28,6 +28,38 @@ class SpellDetail(View):
             {
                 "post": post,
                 "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_area": CommentArea()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = PostSpell.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_area = CommentArea(data=request.POST)
+
+        if comment_area.is_valid():
+            comment_area.instance.email = request.user.email
+            comment_area.instance.username = request.user.username
+            comment = comment_area.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_area = CommentArea()
+
+        return render(
+            request,
+            "spell_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
                 "liked": liked,
                 "comment_area": CommentArea()
             },
