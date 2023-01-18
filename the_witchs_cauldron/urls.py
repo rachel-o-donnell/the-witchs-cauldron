3255312fcccb4a84f48bdb091b38fc4f34d488ac
rachel-o-donnell <hellotherachelo@gmail.com>
2023-01-18 09:31:19 +0000

    Add like functionality
    Created Like view code in views.py, template to render in views.py
    and connected with urls.py

--- the_witchs_cauldron/urls.py
+++ the_witchs_cauldron/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.SpellDetail.as_view(), name='spell_detail'),
+    path('like/<slug:slug>/', views.SpellLike.as_view(), name='spell_like'),
 ]
