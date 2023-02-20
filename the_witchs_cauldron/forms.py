8bb0416c22ee530a085c9e3568e33e2799ad12d6
rachel-o-donnell <hellotherachelo@gmail.com>
2023-02-20 18:21:51 +0000

    Add AddSpell feature and connections
    Forms.py, add_spell.html, and urls.py

--- the_witchs_cauldron/forms.py
+++ the_witchs_cauldron/forms.py
@@ -1,5 +1,6 @@
-from .models import Comment
+from .models import Comment, PostSpell
 from django import forms
+from django_summernote.widgets import SummernoteWidget
 
 
 class CommentArea(forms.ModelForm):
@@ -12,3 +13,24 @@ class EditComment(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+
+class SpellForm(forms.ModelForm):
+    class Meta:
+        model = PostSpell
+        fields = (
+            'title', 'description', 'categories', 'items_needed', 'content', 'main_image')
+
+        widgets = {
+            'description': SummernoteWidget(),
+            'items_needed': SummernoteWidget(),
+            'content': SummernoteWidget(),
+            }
+
+    def __init__(self, *args, **kwargs):
+        super(SpellForm, self).__init__(*args, **kwargs)
+        self.fields['main_image'].label = "Upload your spell image here"
+
+    def __init__(self, *args, **kwargs):
+        super(SpellForm, self).__init__(*args, **kwargs)
+        self.fields['categories'].help_text = "Hold command while clicking to select multiple categories"
--- the_witchs_cauldron/urls.py
+++ the_witchs_cauldron/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('add_spell/', views.AddSpell.as_view(), name='add_spell'),
     path('<slug:slug>/', views.SpellDetail.as_view(), name='spell_detail'),
     path('like/<slug:slug>/', views.SpellLike.as_view(), name='spell_like'),
     path('categories/<category>', views.ListCategories.as_view(),
